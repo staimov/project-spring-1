4dbb1145f00992e21570727b25f45663438ded94
staimov <staimov@mail.ru>
2023-09-15 13:18:55 +0500

    guava added

--- src/main/java/org/staimov/dao/TaskDAO.java
+++ src/main/java/org/staimov/dao/TaskDAO.java
@@ -1,4 +1,22 @@
 package org.staimov.dao;
 
+import org.staimov.domain.Task;
+
+import java.util.List;
+
 public interface TaskDAO {
+
+    Task getById(int id);
+
+    List<Task> getAll();
+
+    int count();
+
+    List<Task> getPage(int offset, int count);
+
+    void createOrUpdate(Task task);
+
+    void delete(Task task);
+
+    void deleteById(int id);
 }
