2d26755b7f73431009d6e32fe9a09108f068fc75
staimov <staimov@mail.ru>
2023-09-17 14:03:49 +0500

    DB_URL updated for docker app

--- src/main/java/org/staimov/config/DBConstants.java
+++ src/main/java/org/staimov/config/DBConstants.java
@@ -6,10 +6,10 @@ public class DBConstants {
     public static final String DB_DRIVER = "com.p6spy.engine.spy.P6SpyDriver";
 
     // for running the app locally
-    public static final String DB_URL = "jdbc:p6spy:mysql://localhost:63306/todo";
+    //public static final String DB_URL = "jdbc:p6spy:mysql://localhost:63306/todo";
 
     // for running the app and mysql as docker containers
-    //public static final String DB_URL = "jdbc:p6spy:mysql://my-mysql:3306/todo";
+    public static final String DB_URL = "jdbc:p6spy:mysql://my-mysql:3306/todo";
 
     public static final int MAX_POOL_SIZE = 10;
     public static final String DB_DIALECT = "org.hibernate.dialect.MySQL8Dialect";
